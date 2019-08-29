e56501f1b6b6ed20bced5beb73068959368d4cb9
mabo <mabo.daniel@gmail.com>
2019-08-28 23:11:38 -0600

    Implementado el cambio de los glifos y escalas ajustables

--- scripts/graph.js
+++ scripts/graph.js
@@ -1,58 +1,86 @@
-/*
- * Mover a un archivo aparte
- */
- 
 var data = [
-  {'x': 42,'y': 42,'r':8},
-  {'x':12,'y':45,'r':10},
-  {'x':56,'y':22,'r':20},
-  {'x': 80,'y':90,'r':20},
-  {'x':33,'y':32,'r':10,'fill':"red"}
+  {'x':42,'y':42,'r':2,'fill':"green"},
+  {'x':32,'y':33,'r':30,'fill':"tomato"},
+  {'x':45,'y':76,'r':24,'fill':"cyan"},
+  {'x':68,'y':72,'r':55,'fill':"purple"},
+  {'x':24,'y':73,'r':32,'fill':"black"},
+  {'x':82,'y':65,'r':20,'fill':"red"}
 ]; //filler data, cambiar a un tsv 
+
+
 /*
-var container = d3.select("body").append("div").classed("svg-container",true);
-var canvas = d3.select(".svg-container")
-              .append("svg")
-                .attr("width","400px")
-                .attr("height","400px")
-                .attr("color","red");
-*/
+ *Objeto global con la configuración acual del plot
+ */
+var configOp = {
+  "fig" : d3.symbolCircle
+}
 
+/* Escalas de visualizacion, el rango se controla con los sliders*/
 var x = d3.scaleLinear()
-               .domain([0,d3.max(data,d=>{return d.x;})])
-               .range([0,200]);
+           .domain([0,d3.max(data,d=>{return d.x;})])
+           .range([0,200]);
 var y = d3.scaleLinear()
-  .domain([0,d3.max(data,d=>{return d.y;})])
-  .range([0,200]);
+           .domain([0,d3.max(data,d=>{return d.y;})])
+           .range([0,200]);
 
+/*Ajsta las propiedades del canvas/plot*/
 var canvas = d3.select(".chart")
                .attr("width","100%")
                .attr("height","100%")
                .attr("viewbox","0 0 420 420");
 
+/*
+ *Esta función se encarga de crear y graficar el plot inicialmente
+ *Todo cambio posterior se realiza por medio de joins
+ */
+function grafica(){
+  var symbol = d3.symbol();
+  d3.select(".chart").selectAll('path')
+   .data(data)
+   .enter()
+    .append('path')
+      .classed("gliph",true)
+      .attr("transform",d=>{return "translate("+x(d.x)+','+y(d.y)+')'})
+      .attr("fill",d=>{return d.fill?d.fill:"blue"})
+      .attr("d",symbol.type(d=>{return configOp['fig']}).size(d=>{return d.r*64}));
+}
+
+/*un backgrpund para ver el tamaño real del canvas, posteriormente se elimina y queda el background del div que lo contiene*/
 canvas.append("rect")
+  .classed("back",true)
   .attr("width","100%")
   .attr("height","100%")
-  .attr("fill","chocolate"); //esto para tener un background, para ref visual
+  .attr("fill","lightcyan"); //esto para tener un background, para ref visual
 
-canvas.selectAll("circle")
-  .data(data)
-  .enter()
-    .append("circle")
-      .attr("transform",d=>{return "translate("+x(d.x)+','+y(d.y)+')'})
-      .attr("r",d=>{return d.r})
-      .attr("fill",d=>{return d.fill?d.fill:"blue"});
-
-var xslider = document.getElementById("xslider");
-xslider.oninput = function(){
+/*controller del slider x*/
+$("#xslider").on("input", function(){
   x.range([0,this.value]);
-  canvas.selectAll("circle")
-    .data(data).attr("transform",d=>{return "translate("+x(d.x)+','+y(d.y)+')'});
-};
+  canvas.selectAll('.gliph')
+    .data(data).attr("transform",d=>{return 'translate('+x(d.x)+','+y(d.y)+')'});
+});
 
-var yslider = document.getElementById("yslider");
-yslider.oninput = function(){
+$("#yslider").on("input", function(){
   y.range([0,this.value]);
-  canvas.selectAll("circle")
-    .data(data).attr("transform",d=>{return "translate("+x(d.x)+','+y(d.y)+')'});
-};
+  canvas.selectAll('.gliph')
+    .data(data).attr("transform",d=>{return 'translate('+x(d.x)+','+y(d.y)+')'});
+});
+
+/*
+ *Esta funcion se ecnrga de actualizar el grafo al nuevo simbolo electo del dropdown
+ */
+function cambio(nuevo){
+  if(configOp['fig']===nuevo) return;
+  configOp['fig']=nuevo;
+  var symbol = d3.symbol();
+  canvas.selectAll('.gliph')
+          .data(data)
+           .attr("d",symbol.type(d=>{return configOp['fig']}).size(d=>{return d.r*64}));
+}
+
+/*Los callbacks del dropdown*/
+$("#a_rectangulos").on("click", ()=>{cambio(d3.symbolSquare);});
+$("#a_burbujas").on("click", ()=>{cambio(d3.symbolCircle);});
+$("#a_glifos").on("click", ()=>{cambio(d3.symbolWye);});
+
+
+grafica();
